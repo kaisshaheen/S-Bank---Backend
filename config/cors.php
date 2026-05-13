85e60d8f4e063205561fd223eae15713ec22f0f3
Kais Shaheen <kaeessh22@gmail.com>
2026-05-13 19:42:21 +0300

    editing

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://s-bank.netlify.app',)],
+    'allowed_origins' => ['https://s-bank.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ Route::post('/register' , [AuthController::class , 'register']);
 Route::post('/login' , [AuthController::class , 'login']);
 Route::post('/logout' , [AuthController::class , 'logout'])->middleware(["auth:sanctum"]);
 
-
+Route::get('/test-session', function () {
+    return session()->all();
+});
 
 //verify the user's email
 Route::get('/email/verify/{id}/{hash}', [VerificationEmailController::class , 'verify'])->middleware(['signed' ])->name('verification.verify');
